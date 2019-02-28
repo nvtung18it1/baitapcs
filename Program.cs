60600162905b862c22574046ee8636877e5b12b9
nvtung18it1 <48077241+nvtung18it1@users.noreply.github.com>
2019-02-28 10:38:33 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class doantau
+    {
+        int sokhach;
+        string matau;
+        int tien;
+        public void nhaptt()
+        {
+            Console.WriteLine("Nhap ma tau:\t");
+            this.matau = Console.ReadLine();
+            Console.WriteLine("Nhap so luong khach:\t");
+            this.sokhach = Convert.ToInt32(Console.ReadLine());
+         }
+       public void intien()
+        {
+            Console.WriteLine("Ma tau {0} so tien la {1}", this.matau,this.tien);
+        }
+       public void tinhtien()
+       {
+           if (this.sokhach < 100)
+               this.tien = this.sokhach * 100000;
+           else this.tien=this.sokhach*80000;
+          
+      }
+
+        static void Main(string[] args)
+        {
+            doantau a= new doantau();
+            a.nhaptt();
+            a.tinhtien();
+            a.intien();
+            Console.ReadKey();
+        }
+    }
+}
